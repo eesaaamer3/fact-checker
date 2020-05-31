9f899775339dbf2ed7ad363dfa13af99927505a2
Eesa <eesaaamer5@gmail.com>
2020-05-31 00:32:15 -0400

    Parsed the requests

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import urllib 
 import requests 
-import bs4 as BeautifulSoup
+from bs4 import BeautifulSoup
 
 # desktop user-agent so urllib returns results from desktop version 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
 query = input("Enter in your fact: ")
 query = query.replace(" ", "+")
-url = "https://google.com/search?q={}".format(query)
+url = 'https://google.com/search?q="{}"'.format(query)
 
 headers = {"user-agent" : USER_AGENT}
 resp = requests.get(url, headers=headers)
 
 if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
+    #print(soup)
 else:
     print("ERROR: There was an issue while searching. Please try again")
 
+results = []
+for g in soup.find_all('div', class_='r'):
+    anchors = g.find_all('a')
+    if anchors:
+        link = anchors[0]['href']
+        title = g.find('h3').text
+        item = {
+            "title": title,
+        }
+        results.append(item)
+print(results)
