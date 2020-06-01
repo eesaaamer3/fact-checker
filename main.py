e454ad9904f7a8252f357754bc0ce5abaf32bea5
Eesa <eesaaamer5@gmail.com>
2020-06-01 19:03:49 -0400

    Reorganized code to be modular

--- main.py
+++ main.py
@@ -4,28 +4,34 @@ from bs4 import BeautifulSoup
 
 # desktop user-agent so urllib returns results from desktop version 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
-
-query = input("Enter in your fact: ")
-query = query.replace(" ", "+")
-url = 'https://google.com/search?q="{}"'.format(query)
-
 headers = {"user-agent" : USER_AGENT}
-resp = requests.get(url, headers=headers)
 
-if resp.status_code == 200:
-    soup = BeautifulSoup(resp.content, "html.parser")
-    #print(soup)
-else:
-    print("ERROR: There was an issue while searching. Please try again")
+def intro():
+    query = input("Enter in your fact: ")
+    query = query.replace(" ", "+")
+    url = 'https://google.com/search?q="{}"'.format(query)
+    html_retrivel(url)
 
-results = []
-for elements in soup.find_all('div', class_='r'):
-    links = elements.find_all('a')
-    if links:
-        link = links[0]['href']
-        title = elements.find('h3').text
-        item = {
-            "title": title,
-        }
-        results.append(item)
-print(len(results))
+def html_retrivel(u):
+    resp = requests.get(u, headers=headers)
+
+    if resp.status_code == 200:
+        html_parser(BeautifulSoup(resp.content, "html.parser"))
+        #print(soup)
+    else:
+        print("ERROR: There was an issue while searching. Please try again")
+
+def html_parser(soup): 
+    results = []
+    for elements in soup.find_all('div', class_='r'):
+        links = elements.find_all('a')
+        if links:
+            link = links[0]['href']
+            title = elements.find('h3').text
+            item = {
+                "title": title,
+            }
+            results.append(item)
+    print(len(results))
+
+intro()
